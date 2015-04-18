f873a9501ce9db7a139b6ea140633b46c0a46dd8
Sami Keijonen <sami.keijonen@foxnet.fi>
2015-04-18 15:09:46 +0300

    Version pumb to 1.0.1.

--- functions.php
+++ functions.php
@@ -8,7 +8,7 @@
 /**
  * The current version of the theme.
  */
-define( 'TOIVO_LITE_VERSION', '1.0.0' );
+define( 'TOIVO_LITE_VERSION', '1.0.1' );
 
 if ( ! function_exists( 'toivo_lite_setup' ) ) :
 /**
