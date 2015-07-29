b0942dca933724c097f9aa6c03765d6c98a4f755
Sami Keijonen <sami.keijonen@foxnet.fi>
2015-07-29 21:23:46 +0300

    Correct theme text domain.

--- loop-meta.php
+++ loop-meta.php
@@ -14,8 +14,8 @@
 			$toivo_loop_desc     = get_post_field( 'post_content', get_queried_object_id(), 'raw' );
 		elseif( is_search() ) :
 			/* Translators: %s is the search query. The HTML entities are opening and closing curly quotes. */
-			$toivo_archive_title = sprintf( __( 'Search results for &#8220;%s&#8221;', 'toivo' ), get_search_query() );
-			$toivo_loop_desc     = sprintf( __( 'You are browsing the search results for &#8220;%s&#8221;', 'toivo' ), get_search_query() );;
+			$toivo_archive_title = sprintf( __( 'Search results for &#8220;%s&#8221;', 'toivo-lite' ), get_search_query() );
+			$toivo_loop_desc     = sprintf( __( 'You are browsing the search results for &#8220;%s&#8221;', 'toivo-lite' ), get_search_query() );
 		elseif( is_author() ) :
 			$toivo_archive_title = get_the_archive_title();
 			$toivo_loop_desc     = get_the_author_meta( 'description', get_query_var( 'author' ) );
