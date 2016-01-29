fdd91ea76da107b0b772b2a740c6eb47edc390ea
Sami Keijonen <sami.keijonen@foxnet.fi>
2016-01-29 10:20:34 +0200

    Remove extra span.

--- comment.php
+++ comment.php
@@ -25,7 +25,7 @@
 				<?php comment_text(); ?>
 				
 				<p class="comment-meta">
-					<cite class="comment-author" <?php hybrid_attr( 'comment-author' ); ?>><?php comment_author_link(); ?></span></cite>
+					<cite class="comment-author" <?php hybrid_attr( 'comment-author' ); ?>><?php comment_author_link(); ?></cite>
 					<a class="comment-permalink" <?php hybrid_attr( 'comment-permalink' ); ?> href="<?php echo esc_url( get_comment_link( $comment->comment_ID, $args ) ); ?>"><time class="comment-published" datetime="<?php comment_time( 'c' ); ?>" <?php hybrid_attr( 'comment-published' ); ?>><?php printf( _x( '%1$s', '%1%s is for comment date', 'toivo-lite' ), get_comment_date() ); ?></time></a>
 					<?php comment_reply_link( array_merge( $args, array( 'add_below' => 'div-comment', 'depth' => $depth, 'max_depth' => $args['max_depth'], 'before' => '<span class="reply">', 'after' => '</span><!-- .reply -->' ) ) ); ?>
 					<?php edit_comment_link(); ?>
